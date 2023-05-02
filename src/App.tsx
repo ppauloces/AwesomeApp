504ef950d42bc140e03d6f35077c9649009d3480
Paulo Amaral <ppaulo.developer@gmail.com>
2023-05-02 18:09:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text } from "react-native"
+
+
+const App = () => {
+
+    return (
+        <SafeAreaView>
+            <Text>Olá</Text>
+        </SafeAreaView>
+    )
+};
+
+export default App
